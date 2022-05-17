b1410a79b54554ca740ce156bf61b97aac15f34c
sgyriley <gayoungshim@ewhain.net>
2022-05-17 23:38:30 +0900

    HW May 17th

--- the_movies_app/src/pages/LoginPage.tsx
+++ the_movies_app/src/pages/LoginPage.tsx
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TextField } from '../components/TextField';
 import { useHistory } from 'react-router-dom';
-import data from "../db/data.json";
+import axios from 'axios';
 
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { replace } = useHistory();
 
-  
-  const users = data.users;
-
   const checkUser = () => {
     if (email === "" || password === "") {
       alert("아이디와 비밀번호를 입력해주세요");
       return;
     }
-    for(let i = 0; i < users.length; i++) {
-      if (email === users[i].email && password === users[i].password){
-        alert("로그인 성공");
-        replace('/');
-        return;
-      } else if (email === users[i].email && password !== users[i].password) {
-        alert("비밀번호가 틀립니다");
-        return;
-      }
-    }
-    alert("로그인 실패");
+    axios
+  .post('http://localhost:1337/api/auth/local', {
+    identifier: email,
+    password: password,
+  })
+  .then((response) => {
+    // Handle success.
+    console.log('Well done!');
+    console.log('User token', response.data.jwt);
+    localStorage.setItem("token", response.data.jwt);
+    replace("/");
+  })
+  .catch((error) => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+  });
   }
-
+useEffect(() => {
+  if (localStorage.getItem("token")) { 
+    replace("/");
+  }
+},[])
   return (
     <div className="m-4"> 
     <div className="text-3xl font-bold mb-10">로그인</div>
